68a71f68c90cc4bb913089efadcc96bfff4dcb33
legod <legod@BRICK>
2025-06-16 19:51:43 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project/IDE and VCS Workflow Project.cpp
@@ -0,0 +1,15 @@
+// IDE and VCS Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    char first, middle, last;
+    // Store your initials in these variables
+    first = 'D';
+    middle = 'A';
+    last = 'M';
+    cout << first << middle << last << endl;
+    return 0;
+
+}
