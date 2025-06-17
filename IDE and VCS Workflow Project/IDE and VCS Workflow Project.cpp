f215174dfe731cfa9c8dedd14e53a8306fa3c575
legod <legod@BRICK>
2025-06-16 20:28:50 -0400

    My father's initials

--- IDE and VCS Workflow Project/IDE and VCS Workflow Project.cpp
+++ IDE and VCS Workflow Project/IDE and VCS Workflow Project.cpp
@@ -7,7 +7,7 @@ int main() {
     char first, middle, last;
     // Store your initials in these variables
     first = 'H';
-    middle = 'G';
+    middle = 'U';
     last = 'M';
     cout << first << middle << last << endl;
     return 0;
